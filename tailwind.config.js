0b2f1be7409222dc072ab81495efa8205aeebfb2
husanswe <sulaymonovhusan2002@gmail.com>
2025-09-12 22:43:18 +0500

    assigned custom breakpoint prefix names and replaced all needed breakpoint names

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// @type {import('tailwindcss').Config}
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    screens: {
+        'mobile': '375px',
+        'tablet': '744px'
+        }
+    }
+}
